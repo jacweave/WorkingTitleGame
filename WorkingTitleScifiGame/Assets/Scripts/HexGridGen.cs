f3063ba9930f39dc0accbeeb559168fbdedad714
jacweave <jacweave@indiana.edu>
2015-06-08 22:18:54 -0400

    Folders and organization

---
+++ WorkingTitleScifiGame/Assets/Scripts/HexGridGen.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexGridGen : MonoBehaviour {
+
+    public Sprite testSprite { get; set; }
+    public Texture texture { get; set; }
+	// Use this for initialization
+	void Start () 
+    {
+        testSprite = new Sprite();
+        ;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
