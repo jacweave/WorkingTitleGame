33bd96a7b5138779a7af9549de9b087117996438
cdweaver1 <cdweaver1@eagles.usi.edu>
2015-08-31 01:17:48 -0400

    Main Menu Changes

---
+++ WorkingTitleScifiGame/Assets/Scripts/MouseOverEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseOverEvent : MonoBehaviour
+{
+
+    public Text myText;
+
+    void OnMouseOver()
+    {
+        myText.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        myText.color = Color.blue;
+    }
+    //
+}
